5bc5eda5bbc61c2b5d818d516786c04bb431985d
Dave Lucia <dlucia@bloomberg.net>
2015-09-06 23:47:30 -0400

    [DL] Progress towards some sort of design

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function () {
-    gulp.src("./app/components/sass/*.scss")
+    gulp.src("./app/components/style.scss")
         .pipe(sass())
-        .pipe(gulp.dest("./public/css"));
+        .pipe(gulp.dest("./public/stylesheets/"));
 });
 
 gulp.task("default", function() {
