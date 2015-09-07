abaafe1fe7d741fbd870954102e434140c673d32
Dave Lucia <dlucia@bloomberg.net>
2015-09-07 11:34:56 -0400

    [DL] Start updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,19 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-gulp.task("sass", function () {
+gulp.task("build-sass", function () {
     gulp.src("./app/components/style.scss")
         .pipe(sass())
         .pipe(gulp.dest("./public/stylesheets/"));
 });
 
-gulp.task("default", function() {
+gulp.task("sass", ["build-sass"], function() {
+    return gulp.watch("./app/components/*.scss", ["build-sass"]);
+});
 
+gulp.task("server", function() {
+});
+
+gulp.task("default", ["sass", "server"], function() {
 });
 
