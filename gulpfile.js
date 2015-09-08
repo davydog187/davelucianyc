fe36ea53fb75a576b1d87ad8587c1b4e00b5519f
Dave Lucia <dlucia@bloomberg.net>
2015-09-07 22:51:10 -0400

    [DL] Working gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var spawn = require("child_process").spawn;
+var node;
 
 gulp.task("build-sass", function () {
     gulp.src("./app/components/style.scss")
@@ -14,8 +16,35 @@ gulp.task("sass", ["build-sass"], function() {
 });
 
 gulp.task("server", function() {
+    if (node) {
+        node.kill();
+    }
+
+    node = spawn("node", ["app.js"], { stdio: "inherit" });
+
+    node.on("close", function (exitCode) {
+        if (exitCode === 8) {
+            gulp.log("Error detected, waiting for changes...");
+        }
+    });
 });
 
-gulp.task("default", ["sass", "server"], function() {
+gulp.task("default", ["sass"], function() {
+    gulp.run("server");
+
+    gulp.watch(["./app.js", "./app/code/**/*.js"], function() {
+        gulp.run("server");
+    });
+
+    gulp.watch("./public/style.css", function() {
+        gulp.run("server");
+    });
+
 });
+
+process.on("exit", function() {
+    if (node) {
+        node.kill();
+    }
+})
 
